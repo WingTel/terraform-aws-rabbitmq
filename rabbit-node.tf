c614dc1e1898fd03563c60c7b1a5bcc050587268
Angie Ellis <angie@ellissoft.com>
2021-11-19 10:57:36 +1000

    variable name fix

--- rabbit-node.tf
+++ rabbit-node.tf
@@ -5,17 +5,17 @@ data "template_file" "rabbit-node" {
   template = file("${path.module}/user_data/rabbitmq.sh")
 
   vars = {
-    AWS_REGION        = var.region
-    VPC_ID            = var.vpc_id
-    ERL_SECRET_COOKIE = var.erl_secret_cookie
-    AWS_ACCESS_KEY    = var.aws_access_key
-    AWS_SECRET_KEY    = var.aws_secret_key
-    RABBITMQ_VERSION  = var.rabbitmq_version
-    ERLANG_VERSION    = var.erlang_version
-    CLUSTER_NAME      = "${var.cluster_fqdn}-${var.name}-${var.environment}"
-    DEFAULT_USER      = var.rabbit_default_user
-    DEFAULT_PASS      = var.rabbit_default_password
-    SSM_CW_CONFIG     = aws_ssm_parameter.cw_agent.name
+    AWS_REGION            = var.region
+    VPC_ID                = var.vpc_id
+    ERL_SECRET_COOKIE     = var.erl_secret_cookie
+    AWS_ACCESS_KEY        = var.aws_access_key
+    AWS_SECRET_KEY        = var.aws_secret_key
+    RABBITMQ_VERSION      = var.rabbitmq_version
+    ERLANG_VERSION        = var.erlang_version
+    CLUSTER_NAME          = "${var.cluster_fqdn}-${var.name}-${var.environment}"
+    DEFAULT_USER          = var.rabbit_default_user
+    DEFAULT_PASS          = var.rabbit_default_password
+    SSM_CLOUDWATCH_CONFIG = aws_ssm_parameter.cw_agent.name
   }
 }
 
