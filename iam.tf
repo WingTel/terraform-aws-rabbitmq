a402124634d063cc89d7e5d272a2cc19429b4c19
Angie Ellis <angie@ellissoft.com>
2021-11-19 16:39:03 +1000

    iam fixes and cleanup

--- iam.tf
+++ iam.tf
@@ -1,27 +1,27 @@
 # Policies
 resource "aws_iam_role" "ProxyRole" {
-  name               = "${var.name}-rabbit-${var.environment}"
+  name               = "${var.name}-rabbit-${var.environment}-role"
   assume_role_policy = file("${path.module}/policies/ProxyRole.json")
 }
 
 resource "aws_iam_instance_profile" "ProxyInstanceProfile" {
-  name = "${var.name}-rabbit-${var.environment}"
+  name = "${var.name}-rabbit-${var.environment}-profile"
   role = aws_iam_role.ProxyRole.name
 }
 
 resource "aws_iam_role_policy" "ProxyPolicies" {
-  name   = "${var.name}-rabbit-${var.environment}"
+  name   = "${var.name}-rabbit-${var.environment}-proxy"
   policy = file("${path.module}/policies/ProxyPolicies.json")
   role   = aws_iam_role.ProxyRole.name
 }
 
 resource "aws_iam_role_policy" "CloudwatchPolicies" {
-  name   = "${var.name}-rabbit-${var.environment}"
+  name   = "${var.name}-rabbit-${var.environment}-cw-policies"
   policy = file("${path.module}/policies/CloudwatchPolicies.json")
   role   = aws_iam_role.ProxyRole.name
 }
 
-# SSM Agent Policies
+# SSM Agent Policies for Cloudwatch Logging
 locals {
   role_policy_arns = [
     "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM",
@@ -29,53 +29,24 @@ locals {
   ]
 }
 
-resource "aws_iam_instance_profile" "this" {
-  name = "EC2-Profile"
-  role = aws_iam_role.this.name
+resource "aws_iam_instance_profile" "CloudwatchLogInstanceProfile" {
+  name = "${var.name}-rabbit-${var.environment}-cw-log-profile"
+  role = aws_iam_role.CloudwatchRole.name
 }
 
 resource "aws_iam_role_policy_attachment" "this" {
   count = length(local.role_policy_arns)
-
-  role       = aws_iam_role.this.name
+  role       = aws_iam_role.CloudwatchLogRole.name
   policy_arn = element(local.role_policy_arns, count.index)
 }
 
-resource "aws_iam_role_policy" "this" {
-  name = "EC2-Inline-Policy"
-  role = aws_iam_role.this.id
-  policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Effect" : "Allow",
-          "Action" : [
-            "ssm:GetParameter"
-          ],
-          "Resource" : "*"
-        }
-      ]
-    }
-  )
+resource "aws_iam_role_policy" "CloudwatchLogPolicies" {
+  name = "${var.name}-rabbit-${var.environment}-cw-log-policies"
+  role = aws_iam_role.CloudwatchLogRole.id
+  policy = file("${path.module}/policies/CloudwatchLogPolicies.json")
 }
 
-resource "aws_iam_role" "this" {
-  name = "EC2-Role"
-  path = "/"
-
-  assume_role_policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Action" : "sts:AssumeRole",
-          "Principal" : {
-            "Service" : "ec2.amazonaws.com"
-          },
-          "Effect" : "Allow"
-        }
-      ]
-    }
-  )
+resource "aws_iam_role" "CloudwatchLogRole" {
+  name = "${var.name}-rabbit-${var.environment}-cw-log-profile"
+  assume_role_policy = file("${path.module}/policies/CloudwatchRole.json")
 }
