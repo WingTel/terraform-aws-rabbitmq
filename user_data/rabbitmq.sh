70ca971e286124a85916d8675b18b1ddc2120b00
Angie Ellis <angie@ellissoft.com>
2021-11-19 19:51:47 +1000

    override cloudwatch agent log path

--- user_data/rabbitmq.sh
+++ user_data/rabbitmq.sh
@@ -169,7 +169,7 @@ apt-get install collectd -y
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 dpkg -i amazon-cloudwatch-agent.deb
 
-cat << EndOfConfig >> /opt/aws/amazon-cloudwatch-agent/etc/agent-config.json
+cat << EndOfConfig >> /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 {
   "agent": {
     "metrics_collection_interval": 10,
@@ -195,4 +195,4 @@ cat << EndOfConfig >> /opt/aws/amazon-cloudwatch-agent/etc/agent-config.json
 }
 EndOfConfig
 
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon -s
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
